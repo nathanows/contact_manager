7fe72a70aa4d9ea38311c3e5e67a27a2c22d5d8a
Nathan Owsiany <nowsiany@gmail.com>
2015-01-06 20:41:10 -0700

    added validation tests to email address

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe EmailAddress, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:email_address) { EmailAddress.new(address: 'nowsiany@gmail.com', person_id: 1) }
+
+  it 'is valid' do
+    expect(email_address).to be_valid
+  end
+
+  it 'is invalid without an email address' do
+    email_address.address = nil
+    expect(email_address).to_not be_valid
+  end
+
+  it 'is invalid without a person_id' do
+    email_address.person_id = nil
+    expect(email_address).to_not be_valid
+  end
 end
